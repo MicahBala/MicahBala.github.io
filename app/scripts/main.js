af050da98124ee0a8b3a517f0a877ab86de09aa1
Micah Yunana Bala <micahkulien@gmail.com>
2018-11-06 19:15:02 +0100

    Finish home page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+// let menuNav = document.querySelector('.menu-nav');
+// let navItems = document.querySelectorAll('.van-item');
+
+// Set Initial State of menu
+let showMenu = false;
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+
+    showMenu = true;
+    // menuNav.classList.add('show');
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+
+    showMenu = false;
+  }
+}
+
+menuBtn.addEventListener('click', toggleMenu);
